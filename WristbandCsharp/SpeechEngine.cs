0e36561e930cdc3fea76a2964e717a6dc51110d9
unknown <hfs5022@ALPHAREALM.cse.psu.edu>
2014-11-08 10:14:57 -0500

    Adds speechengine file.

---
+++ WristbandCsharp/SpeechEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Speech
+{
+    class SpeechEngine
+    {
+        SpeechSynthesizer sSynth;
+        PromptBuilder pBuilder;
+
+        public SpeechEngine()
+        {
+            Initialize();            
+        }
+
+        private void Initialize()
+        {
+            sSynth = new SpeechSynthesizer();
+            pBuilder = new PromptBuilder();
+        }
+
+        public void Speak(string speechString)
+        {
+            pBuilder.ClearContent();
+            pBuilder.AppendText(speechString);
+            sSynth.Speak(pBuilder);
+        }
+
+    }
+}
