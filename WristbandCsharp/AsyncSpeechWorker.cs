9b2dd490c758206aaaa0009b4311716e2e8d1ccf
unknown <hfs5022@ALPHAREALM.cse.psu.edu>
2014-11-08 11:17:50 -0500

    Adds class

---
+++ WristbandCsharp/AsyncSpeechWorker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WristbandCsharp
+{
+
+    
+
+    class AsyncSpeechWorker
+    {
+        private volatile bool shouldStop = false;
+        private volatile string direction = "";
+        private Speech.SpeechEngine se;
+
+        public AsyncSpeechWorker()
+        {
+            se = new Speech.SpeechEngine();
+        }
+
+        public void doWork()
+        {
+            while (!shouldStop)
+            {
+                if (direction != "") se.SpeakAsync(direction);
+            }
+        }
+
+        public void requestStop()
+        {
+            shouldStop = true;
+        }
+        public void setDirection(string direction) {
+            this.direction = direction;
+        }
+
+    }
+
+   
+}
